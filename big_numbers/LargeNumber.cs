6ab72a504c35737fe57a3194c1ca5dc1c9c2e19e
fwiffo <adskaya.taburetka@gmail.com>
2015-10-18 23:34:32 +0300

    todo decimal

---
+++ big_numbers/LargeNumber.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Numerics;
+
+namespace big_numbers
+{
+	public class LargeNumber
+	{
+		private BigInteger _numerator;
+		private BigInteger _denominator;
+
+		public BigInteger Denominator
+		{ 
+			get { return _denominator; }
+			set 
+			{ 
+				if (value == 0)
+					throw new ArgumentException ();
+				else
+					_denominator = value;
+				CutFraction ();
+			} 
+		}
+
+		public BigInteger Numerator{ 
+			get{ return _numerator; } 
+			set
+			{ 
+				_numerator = value; 
+				CutFraction ();
+			}
+		}
+
+		public LargeNumber () : this (0, 1)
+		{
+		}
+
+		public LargeNumber (BigInteger numerator, BigInteger denominator)
+		{
+			Denominator = denominator;
+			Numerator = numerator;
+			CutFraction ();
+		}
+
+		public static LargeNumber operator + (LargeNumber n1, LargeNumber n2)
+		{
+			BigInteger numerator, denominator;
+
+			if (n1.Denominator != n2.Denominator)
+			{
+				denominator = n1.Denominator * n2.Denominator;
+				numerator = n1.Numerator * n2.Denominator + n2.Numerator * n1.Denominator;
+			}
+			else
+			{
+				denominator = n1.Denominator;
+				numerator = n1.Numerator + n2.Numerator;
+			}
+			var result = new LargeNumber (numerator, denominator);
+			result.CutFraction ();
+			return result;
+		}
+
+		public static LargeNumber operator - (LargeNumber n1, LargeNumber n2)
+		{
+			BigInteger numerator, denominator;
+
+			if (n1.Denominator != n2.Denominator)
+			{
+				denominator = n1.Denominator * n2.Denominator;
+				numerator = n1.Numerator * n2.Denominator - n2.Numerator * n1.Denominator;
+			}
+			else
+			{
+				denominator = n1.Denominator;
+				numerator = n1.Numerator - n2.Numerator;
+			}
+			var result = new LargeNumber (numerator, denominator);
+			result.CutFraction ();
+			return result;
+		}
+
+		public static LargeNumber operator / (LargeNumber n1, LargeNumber n2)
+		{
+			BigInteger numerator, denominator;
+			numerator = n1.Numerator * n2.Denominator;
+			denominator = n2.Numerator * n1.Denominator;
+			LargeNumber result = new LargeNumber (numerator, denominator);
+			result.CutFraction ();
+			return result;
+		}
+
+		public static LargeNumber operator * (LargeNumber n1, LargeNumber n2)
+		{
+			if (n1.Numerator == 0 || n2.Numerator == 0)
+			{
+				return new LargeNumber (0, 1);
+			}
+			BigInteger numerator, denominator;
+			numerator = n1.Numerator * n2.Numerator;
+			denominator = n1.Denominator * n2.Denominator;
+			var result = new LargeNumber (numerator, denominator);
+			result.CutFraction ();
+			return result;
+		}
+		public void CutFraction()
+		{
+			BigInteger greatestCommonDivisor = BigInteger.GreatestCommonDivisor (Numerator, Denominator);
+			_numerator /= greatestCommonDivisor;
+			_denominator /= greatestCommonDivisor;
+			if (_denominator < 0)
+			{
+				_numerator *= -1;
+				_denominator *= -1;
+			}
+		}
+		public override string ToString ()
+		{
+			if (Denominator == 1)
+				return Numerator.ToString ();
+			return String.Format ("({0}/{1})", Numerator, Denominator);
+		}
+		public string printDecimal()
+		{
+			return "";
+		}
+		public static bool operator ==(LargeNumber n1, LargeNumber n2)
+		{
+			return n1.Numerator * n2.Denominator == n2.Numerator * n1.Denominator;
+		}
+		public static bool operator >(LargeNumber n1, LargeNumber n2)
+		{
+			return n1.Numerator * n2.Denominator > n2.Numerator * n1.Denominator;
+		}
+		public static bool operator <(LargeNumber n1, LargeNumber n2)
+		{
+			return n1.Numerator * n2.Denominator < n2.Numerator * n1.Denominator;
+		}
+		public static bool operator <=(LargeNumber n1, LargeNumber n2)
+		{
+			return n1.Numerator * n2.Denominator <= n2.Numerator * n1.Denominator;
+		}
+		public static bool operator >=(LargeNumber n1, LargeNumber n2)
+		{
+			return n1.Numerator * n2.Denominator >= n2.Numerator * n1.Denominator;
+		}
+		public static bool operator !=(LargeNumber n1, LargeNumber n2)
+		{
+			return n1.Numerator * n2.Denominator != n2.Numerator * n1.Denominator;
+		}
+		public string printDecimal()
+		{
+
+		}
+		//todo beskonechnaya desyatichnaya drobj
+	}
+}
+
---
+++ big_numbers/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace big_numbers
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			LargeNumber n1 = new LargeNumber (-5, 0);
+			LargeNumber n2 = new LargeNumber (8, -6);
+			Console.WriteLine (n1);
+			Console.WriteLine (n1 == n2);
+		}
+	}
+}
